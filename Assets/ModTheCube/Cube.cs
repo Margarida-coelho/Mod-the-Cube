55eb76f6a59170c3f4c2ca9b3d1428dcc246c131
Margarida-coelho <162127564+Margarida-coelho@users.noreply.github.com>
2024-03-06 14:32:40 +0000

    Mod The Cube Final Commit

---
+++ Assets/ModTheCube/Cube.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+
+    Vector3 originalScale;
+
+    Color originalColor;
+    void Start()
+    {
+        Material material = Renderer.material;
+        originalColor = material.color; 
+        originalScale = transform.localScale;
+        StartCoroutine(ChangeCube());        
+        
+
+    }
+    
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * 20);
+    }
+
+    IEnumerator ChangeCube()
+    {
+        while (true)
+        {
+            Material material = Renderer.material;
+
+            if (transform.position.z != 6.5f)
+            {
+                transform.position = new Vector3(0, 0 , transform.position.z + .5f);
+                transform.localScale = new Vector3(transform.localScale.x + .1f, transform.localScale.y + .1f, transform.localScale.z + .1f);
+                material.color = new Color(material.color.r + .15f, material.color.g, material.color.b, material.color.a - .5f);
+                yield return new WaitForSeconds(.5f);
+            }
+            else
+            {
+                transform.position = new Vector3(0, 0, -6.5f);
+                transform.localScale = originalScale;
+                material.color = originalColor;
+            }
+        }
+    }
+}
